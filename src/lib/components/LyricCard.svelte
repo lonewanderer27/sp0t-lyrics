5986d21e93bfa6dc571609396f036dd1b0fb88c9
lonewanderer27 <adrianejames27@gmail.com>
2024-05-14 05:23:04 +0800

    feat: Add html-to-image dependency for generating lyric card images

---
+++ src/lib/components/LyricCard.svelte
@@ -0,0 +1,38 @@
+<script lang="ts">
+	import type { LyricType } from '../../types';
+	export let headerImageThumbnailUrl: string;
+	export let title: string;
+	export let artistNames: string;
+	export let selectedLyrics: LyricType[];
+	export let showSpotifyLogo: boolean;
+</script>
+
+<div id="lyric-card-container" class="p-10 flex flex-col justify-center">
+	<div id="lyric-card" class="lyric-card p-5 shadow-xl bg-base-100 rounded-xl">
+		<div class="lyric-card-header flex">
+			<div class="lyric-card-image mr-4">
+				<img src={headerImageThumbnailUrl} alt={title} class="w-10 h-auto rounded-sm" />
+			</div>
+			<div>
+				<p class="lyric-card-title font-semibold truncate">{title}</p>
+				<p class="lyric-card-artist text-xs">{artistNames}</p>
+			</div>
+		</div>
+		<div class="lyric-card-body mt-5">
+			{#each selectedLyrics as lyric}
+				<p class="lyric-card-line text-2xl font-bold my-2">
+					{lyric.text}
+				</p>
+			{/each}
+		</div>
+		{#if showSpotifyLogo}
+			<div class="lyric-card-footer mt-5 grayscale">
+				<img
+					src="/spotify_logo_with_text.png"
+					style="width: 5rem; height: auto"
+					alt="Spotify Logo"
+				/>
+			</div>
+		{/if}
+	</div>
+</div>
--- src/routes/share/[id]/+page.svelte
+++ src/routes/share/[id]/+page.svelte
@@ -1,13 +1,14 @@
 <script lang="ts">
 	export let song;
-  import type { LyricType } from "../../../types/index.d.ts";
-
+	import type { LyricType } from '../../../types/index.d.ts';
+	import { toPng, toJpeg, toBlob, toPixelData, toSvg } from 'html-to-image';
 	import { Navbar, NavbarBackLink, Page, Button } from 'konsta/svelte';
 	import { audioLyricsInfo, selectedLyrics, audioLyrics } from '../../../stores/lyrics';
 	import { goto } from '$app/navigation';
 	import { page } from '$app/stores';
 	import { Icon } from 'svelte-ionicons';
 	import html2canvas from 'html2canvas';
+	import LyricCard from '$lib/components/LyricCard.svelte';
 
 	// execute this code when the page is loaded
 	$: () => {
@@ -77,7 +78,7 @@
 		const viewportContent = viewport.content;
 
 		// set the viewport content to the width of the lyric card
-		viewport.content = 'width=500';
+		viewport.content = 'width=800';
 
 		// fix line height issues
 		const cleanup = fixLineHeightIssues();
@@ -96,17 +97,29 @@
 		return canvas.toDataURL('image/png');
 	};
 
+	const genImage = async () => {
+		const node = document.getElementById('lyric-card-container')!;
+		const dataUrl = await toPng(node, {
+			backgroundColor: 'transparent',
+		});
+
+		// return the data url
+		return dataUrl;
+	};
+
 	const download = async () => {
 		const fakeLink = window.document.createElement('a');
 		fakeLink.style.display = 'none';
 
 		// construct the filename
-    
-    // get the selected lyrics
-    const lyrics = $selectedLyrics.map((lyric: LyricType) => lyric.text);
 
-    // construct a filename from the artist name and the title, and add the selected lyrics
-    const filename = `${$audioLyricsInfo.artist_names} - ${$audioLyricsInfo.title} - lyrics - ${lyrics.join(' ')}`;
+		// get the selected lyrics
+		const lyrics = $selectedLyrics.map((lyric: LyricType) => lyric.text);
+
+		// construct a filename from the artist name and the title, and add the selected lyrics
+		const filename = `${$audioLyricsInfo.artist_names} - ${
+			$audioLyricsInfo.title
+		} - lyrics - ${lyrics.join(' ')}`;
 
 		fakeLink.download = `${filename}.png`;
 		fakeLink.href = await getImage();
@@ -122,30 +135,13 @@
 		</Button>
 	</Navbar>
 
-	<div id="lyric-card-container" class="p-10 flex flex-col justify-center">
-		<div id="lyric-card" class="lyric-card p-5 shadow-xl bg-base-100 rounded-xl">
-			<div class="lyric-card-header flex">
-				<div class="lyric-card-image mr-4">
-					<img
-						src={$audioLyricsInfo.header_image_thumbnail_url}
-						alt={$audioLyricsInfo.title}
-						class="w-10 h-auto rounded-sm"
-					/>
-				</div>
-				<div>
-					<p class="lyric-card-title font-semibold truncate">{$audioLyricsInfo.title}</p>
-					<p class="lyric-card-artist text-xs">{$audioLyricsInfo.artist_names}</p>
-				</div>
-			</div>
-			<div class="lyric-card-body mt-5">
-				{#each $selectedLyrics as lyric}
-					<p class="lyric-card-line text-2xl font-bold my-2">
-						{lyric.text}
-					</p>
-				{/each}
-			</div>
-		</div>
-	</div>
+	<LyricCard
+		headerImageThumbnailUrl={$audioLyricsInfo.header_image_thumbnail_url}
+		title={$audioLyricsInfo.title}
+		artistNames={$audioLyricsInfo.artist_names}
+		selectedLyrics={$selectedLyrics}
+		showSpotifyLogo={true}
+	/>
 
 	<div class="w-full flex justify-center">
 		<div class="grid grid-cols-6 text-center">
